a44df3228c45fefeb899dbdd2941c253001a11a6
Rad Cirskis <nad2000@gmail.com>
2018-12-09 01:07:55 +0900

    data import added...

--- main.go
+++ main.go
@@ -21,14 +21,36 @@ type Joke struct {
 
 var db *gorm.DB
 
+func setUpDB() {
+	te := db.HasTable(&Joke{})
+	db.AutoMigrate(&Joke{})
+	if !te {
+		for _, j := range []Joke{
+			{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+			{2, 0, "What do you call a fake noodle? An Impasta."},
+			{3, 0, "How many apples grow on a tree? All of them."},
+			{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+			{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+			{6, 0, "Why did the coffee file a police report? It got mugged."},
+			{7, 0, "How does a penguin build it's house? Igloos it together."},
+		} {
+			if err := db.Create(&j).Error; err != nil {
+				log.WithError(err).Errorln("Failed to insert a joke", j)
+			}
+		}
+
+	}
+}
+
 func main() {
 	var err error
-	db, err := gorm.Open("sqlite3", "jokes.db")
+	db, err = gorm.Open("sqlite3", "jokes.db")
 
 	if err != nil {
 		log.WithError(err).Panicln("Failed to connect to the DB.")
 	}
 	defer db.Close()
+	setUpDB()
 
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
