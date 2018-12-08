ab655068c87550c4c5fbab89a285d4ab68e346ec
Rad Cirskis <nad2000@gmail.com>
2018-12-09 00:19:28 +0900

    add model

--- main.go
+++ main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 
-  "github.com/gin-gonic/contrib/static"
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
+	log "github.com/sirupsen/logrus"
 )
 
+// Joke contains invormattion about a single Joke
+type Joke struct {
+	ID    int    `json:"id"`
+	Likes int    `json:"likes"`
+	Joke  string `json:"joke"`
+}
+
+var db *gorm.DB
+
 func main() {
-  // Set the router as the default one shipped with Gin
-  router := gin.Default()
+	var err error
+	db, err := gorm.Open("sqlite3", "jokes.db")
 
-  // Serve frontend static files
-  router.Use(static.Serve("/", static.LocalFile("./views", true)))
+	if err != nil {
+		log.WithError(err).Panicln("Failed to connect to the DB.")
+	}
+	defer db.Close()
 
-  // Setup route group for the API
-  api := router.Group("/api")
-  {
-    api.GET("/", func(c *gin.Context) {
-      c.JSON(http.StatusOK, gin.H {
-        "message": "pong",
-      })
-    })
-  }
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
 
-  // Start and run the server
-  router.Run(":3000")
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		api.GET("/jokes", JokeHandler)
+		api.POST("/jokes/like/:jokeID", LikeJoke)
+	}
+
+	// Start and run the server
+	router.Run(":3000")
+}
+
+// JokeHandler retrieves a list of available jokes
+func JokeHandler(c *gin.Context) {
+	c.Header("Context-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "JokeHandler not implemented yet",
+	})
+}
+
+// LikeJoke increments the likes of a particular joke Item
+func LikeJoke(c *gin.Context) {
+	c.Header("Context-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "LikeJoke not implemented yet",
+	})
 }
